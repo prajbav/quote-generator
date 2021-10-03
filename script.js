4cbc98877a6dea6e9ca904cdf9bf764bff2c5eab
prajakta_cuny <prajakta.bavikar@gmail.com>
2021-10-03 16:34:25 -0400

    new quotes button working

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let quoteContainerId = document.getElementById('quote-container');
-let quoteId = document.getElementById('quote');
-let authorId = document.getElementById('author');
-let twitterBtnId = document.getElementById('twitter');
+let quoteText = document.getElementById('quote');
+let authorText = document.getElementById('author');
+let twitterBtn = document.getElementById('twitter');
+let fbBtn = document.getElementById('fb');
+let newQuoteBtn = document.getElementById('new-quote');
 
 let apiQuotes = [];
 async function getQuotes(){
@@ -20,7 +22,24 @@ getQuotes();
 
 function newQuote(){   
     let quote =  apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    quoteId.textContent = quote.text;
-    authorId.textContent = quote.author;
+    if(quote.text.length > 150){
+        quoteText.classList.add('long-quote');
+    }else{
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text;
+    if(quote.author){
+        authorText.textContent = quote.author;
+    }else{
+        authorText.textContent = 'Unknown';
+    }
     console.log(quote);
 }
+
+function fbPost(){
+    const fbUrl = `https://graph.facebook.com/prajakta.bavikar/feed?message=${quoteText.textContent}&access_token={page-access-token}"`;
+    window.open(fbUrl, '_blank');
+}
+
+newQuoteBtn.addEventListener('click', newQuote);
+fbBtn.addEventListener('click',fbPost);
