ccb399d0a76e5e8fa38742ee7e2810ece503eb89
prajakta_cuny <prajakta.bavikar@gmail.com>
2021-10-03 22:57:48 -0400

    css loader added

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-let quoteContainerId = document.getElementById('quote-container');
-let quoteText = document.getElementById('quote');
-let authorText = document.getElementById('author');
-let twitterBtn = document.getElementById('twitter');
-let fbBtn = document.getElementById('fb');
-let newQuoteBtn = document.getElementById('new-quote');
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const fbBtn = document.getElementById('fb');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
 
 let apiQuotes = [];
 async function getQuotes(){
+    showLoading();
     const apiUrl = 'https://type.fit/api/quotes';
     try {
       const response = await fetch(apiUrl);
@@ -33,11 +36,12 @@ function newQuote(){
     }else{
         authorText.textContent = 'Unknown';
     }
+    complete();
     console.log(quote);
 }
 
 function fbPost(){
-    const fbUrl = `https://graph.facebook.com/prajakta.bavikar/feed?message=${quoteText.textContent}&access_token={page-access-token}"`;
+    const fbUrl = `https://graph.facebook.com/me/feed?message=${quoteText.textContent}&access_token={page-access-token}"`;
     window.open(fbUrl, '_blank');
 }
 
@@ -46,7 +50,14 @@ function tweetQuote(){
     window.open(tweetUrl, '_blank');
 
 }
-
+function showLoading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+function complete(){
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
 newQuoteBtn.addEventListener('click', newQuote);
 //fbBtn.addEventListener('click',fbPost);
 twitterBtn.addEventListener('click',tweetQuote);
